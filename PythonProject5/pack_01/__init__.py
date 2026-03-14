14c909f0bd3a912d4a65501c581abd3200f9b16f
caai9 <1604836917@qq.com>
2026-03-14 15:16:40 +0800

    Delete PythonProject5 directory

--- PythonProject5/pack_01/__init__.py
+++
@@ -1,7 +0,0 @@
-# print('__init__.py')
-    # # 作用导入包内其他模块
-# from pack_01 import qweq
-# qweq.assd()
-__all__=['qweq']    #相当于导入【】里面定义的模块
-
-#包的本质依然是包，包可以包含包
--- PythonProject5/test.py
+++
@@ -1,6 +0,0 @@
-print('会显示')
-def test1():
-    print('hhh')
-
-if __name__ == '__main__':#被当作模块导入时不会执行
-    print('这是test，不会显示')
