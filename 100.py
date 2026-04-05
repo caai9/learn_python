9db1a32f93417cff173ed7314a4c2e037917b897
caai9 <1604836917@qq.com>
2026-04-05 15:41:54 +0800

    每日一学

--- 100.py
+++ 100.py
@@ -1570,3 +1570,121 @@ set2 = {2,4,6,8,10}
 #     return -1
 #
 # print(bin_search(merge_sort(num),57))
+
+
+
+# 公鸡5元一只 母鸡3元一只 小鸡1元三只
+# 用100元买100只鸡 问公鸡/母鸡/小鸡各多少只
+
+# for x in range(20):
+#     for y in range(33):
+#         for z in range(300):
+#             if x+y+z==100 and x*5+y*3+z/3==100:
+#                 print(f'公鸡:{x}只,母鸡{y}只,小鸡:{z}只')
+
+
+
+
+
+# A、B、C、D、E五人在某天夜里合伙捕鱼 最后疲惫不堪各自睡觉
+# 第二天A第一个醒来 他将鱼分为5份 扔掉多余的1条 拿走自己的一份
+# B第二个醒来 也将鱼分为5份 扔掉多余的1条 拿走自己的一份
+# 然后C、D、E依次醒来也按同样的方式分鱼 问他们至少捕了多少条鱼
+
+# fish0=6
+# # (((fish-1)/5)*4)**5
+# cnt=0
+# while True:
+#     fish = fish0
+#     for i in range(5):
+#         if (fish-1)%5==0:
+#             fish = (fish-1)//5*4
+#             cnt+=1
+#         else:
+#             break
+#     if cnt == 5:
+#         print(fish0)
+#         break
+#     cnt=0
+#     fish0+=5
+
+"""
+贪婪法：在对问题求解时，总是做出在当前看来是最好的选择，不追求最优解，快速找到满意解。
+输入：
+20 6
+电脑 200 20
+收音机 20 4
+钟 175 10
+花瓶 50 2
+书 10 1
+油画 90 9
+"""
+
+# class Thing(object):
+#     def __init__(self,name,price,weight):
+#         self.name = name
+#         self.price = price
+#         self.weight = weight
+#     @property
+#     def value(self):
+#         return self.price/self.weight
+# def input_thing():
+#     name_str,price_str,weight_str = input().split()
+#     return name_str,int(price_str),int(weight_str)
+#
+# def main():
+#     max_weight,num_of_things = map(int,input().split())
+#     all_things = []
+#     for _ in range(num_of_things):
+#         all_things.append(Thing(*input_thing()))
+#     all_things.sort(key=lambda x:x.value,reverse=True)
+#     total_weight = 0
+#     total_price = 0
+#     for thing in all_things:
+#         if total_weight + thing.weight<=max_weight:
+#             print(f'小偷拿走了{thing.name}')
+#             total_weight+=thing.weight
+#             total_price+=thing.price
+#     print(f'总价值: {total_price}美元')
+#
+# if __name__=='__main__':
+#     main()
+
+
+
+import sys
+import time
+SIZE=5
+total = 0
+
+def print_board(board):
+    for row in board:
+        for col in row:
+            print(str(col).center(4),end='')
+        print()
+def patrol(board,row,col,step=1):
+    if row>=0 and row<SIZE and \
+        col>=0 and col<SIZE and \
+        board[row][col] == 0:
+        board[row][col] = step
+        if step==SIZE*SIZE:
+            global total
+            total+=1
+            print(f'第{total}种走法: ')
+            print_board(board)
+        patrol(board, row - 2, col - 1, step + 1)
+        patrol(board, row - 1, col - 2, step + 1)
+        patrol(board, row + 1, col - 2, step + 1)
+        patrol(board, row + 2, col - 1, step + 1)
+        patrol(board, row + 2, col + 1, step + 1)
+        patrol(board, row + 1, col + 2, step + 1)
+        patrol(board, row - 1, col + 2, step + 1)
+        patrol(board, row - 2, col + 1, step + 1)
+        board[row][col]=0
+
+def main():
+    board = [[0]*SIZE  for _ in range(SIZE)]
+    patrol(board,SIZE-1,SIZE-1)
+
+if __name__=='__main__':
+    main()
